2add6515a67c5acde03a28eb1eb4c8b9b29862a7
iklaq <iklaq.ahmad@jalantechnologies.com>
2023-04-27 14:52:38 +0530

    added states api

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,38 +5,72 @@ import { useState, useEffect } from "react";
 const Form = () => {
   const [countriesData, setCountriesData] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState();
+  const [statesData,setStatesData] = useState([]);
+  const [selectedState, setSelectedState] = useState(null);
 
+  useEffect(() => {
+    const asyncFn = async () => {
+      const url = "https://city-and-state-search-api.p.rapidapi.com/countries";
+      const options = {
+        method: "GET",
+        headers: {
+          "content-type": "application/octet-stream",
+          "X-RapidAPI-Key":
+            "41adefae10mshcfc84b75a81725bp162718jsne27e167b34c2",
+          "X-RapidAPI-Host": "city-and-state-search-api.p.rapidapi.com",
+        },
+      };
 
-  useEffect(async () => {
-    const url = "https://city-and-state-search-api.p.rapidapi.com/countries";
-    const options = {
-      method: "GET",
-      headers: {
-        "content-type": "application/octet-stream",
-        "X-RapidAPI-Key": "41adefae10mshcfc84b75a81725bp162718jsne27e167b34c2",
-        "X-RapidAPI-Host": "city-and-state-search-api.p.rapidapi.com",
-      },
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+
+        const arr = result.map((el) => el.name);
+        console.log(arr);
+        setCountriesData(arr);
+      } catch (error) {
+        console.error(error);
+      }
     };
 
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-
-      const arr = result.map((id) => id.name);
-
-      setCountriesData(arr);
-    } catch (error) {
-      console.error(error);
-    }
-
-   
+    asyncFn();
   }, []);
 
-  console.log(countriesData);
+  useEffect(() => {
+    if (selectedCountry) {
+      const asyncFn = async () => {
+        const url = `https://city-and-state-search-api.p.rapidapi.com/states?country_name=${selectedCountry}`;
+        const options = {
+          method: "GET",
+          headers: {
+            "content-type": "application/octet-stream",
+            "X-RapidAPI-Key":
+              "41adefae10mshcfc84b75a81725bp162718jsne27e167b34c2",
+            "X-RapidAPI-Host": "city-and-state-search-api.p.rapidapi.com",
+          },
+        };
+
+        try {
+          const response = await fetch(url, options);
+          const result = await response.json();
+          const arr = result.map((el) => el.name);
+          console.log(arr);
+          setSelectedState(null);
+          setStatesData(arr)
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      asyncFn();
+    } 
+    else{
+        setStatesData([]);
+    }
+  }, [selectedCountry]);
 
   return (
     <div className="header">
-      <p>Can you provide your personal details?</p>
+      <p>Can you rovide your personal details?</p>
       <form>
         <div className="inputs">
           <label for="">Name</label>
@@ -55,23 +89,34 @@ const Form = () => {
 
         <div className="inputs">
           <label for="">Country</label>
-          <select name="country"  onChange={(e)=>setSelectedCountry(e.target.value
-          )}
+          <select
+            name="country"
+            onChange={(e) => setSelectedCountry(e.target.value)}
           >
             <optgroup label="Select a country">
-            <option></option>    
-            
-            {countriesData.map((currentElement) => (
-              <option value={currentElement}>{currentElement}</option>
-            ))}
+              <option></option>
 
+              {countriesData.map((currentElement) => (
+                <option value={currentElement}>{currentElement}</option>
+              ))}
             </optgroup>
           </select>
         </div>
 
         <div className="inputs">
           <label for="">State</label>
-          <select name="" id=""></select>
+          <select
+            name="state"
+           onChange={(e)=>setSelectedState(e.target.value)}
+          >
+            <optgroup label="Select a state">
+              <option></option>
+
+              {statesData.map((currentElement) => (
+                <option value={currentElement}>{currentElement}</option>
+              ))}
+            </optgroup>
+          </select>
         </div>
 
         <div className="inputs">
@@ -80,7 +125,6 @@ const Form = () => {
         </div>
 
         <button>Submit</button>
-        <h1>{selectedCountry}</h1>
       </form>
     </div>
   );
